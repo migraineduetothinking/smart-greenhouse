6a7b7dcd89e6d321b4a78cb028d7d4240e3cf7a4
Kyrylo Tereshchenko <terechenko.kirill@gmail.com>
2023-12-10 16:39:58 +0200

    Added comments. Still need to find way to get real data for chart

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -5,27 +5,36 @@ import 'react-datepicker/dist/react-datepicker.css';
 import uk from 'date-fns/locale/uk';
 
 const Header = ({ title, onDateChange }) => {
+  // Хук стану для відстеження відкриття та закриття меню вибору дат
   const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+
+  // Стани для початкової та кінцевої дати
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+
+  // Стани для зберігання попередніх значень дат
   const [prevStartDate, setPrevStartDate] = useState(startDate);
   const [prevEndDate, setPrevEndDate] = useState(endDate);
   
 
-
+  // Обробник натискання на кнопку відкриття/закриття меню вибору дат
   const handleDateButtonClick = () => {
     setIsDatePickerOpen(!isDatePickerOpen);
   };
 
+  // Обробник натискання на кнопку підтвердження
   const handleConfirmButtonClick = () => {
     setIsDatePickerOpen(false);
    
     // Зберігаємо попередні значення
     setPrevStartDate(startDate);
     setPrevEndDate(endDate);
+
+    // Передаємо дати 
     onDateChange(startDate, endDate);
   };
 
+  // Обробник натискання на кнопку скасування
   const handleCancelButtonClick = () => {
     setIsDatePickerOpen(false);
     // Повертаємо попередні значення при скасуванні вибору дат
@@ -33,10 +42,12 @@ const Header = ({ title, onDateChange }) => {
     setEndDate(prevEndDate);
   };
 
+  // Функція для великої літери першої букви в рядку
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
+  // Функція для форматування дати у вигляді "День тижня, число Місяць Рік"
   const formatDate = (date) => {
     const formattedDate = date.toLocaleDateString('uk-UA', {
       weekday: 'short',
@@ -60,6 +71,7 @@ const Header = ({ title, onDateChange }) => {
         </button>
         {isDatePickerOpen && (
           <div className="date-picker-menu">
+            {/* Календар для вибору лівої дати */}
             <DatePicker
               selected={startDate}
               onChange={(date) => setStartDate(date)}
@@ -67,6 +79,7 @@ const Header = ({ title, onDateChange }) => {
               locale={uk}
               maxDate={new Date()} 
             />
+            {/* Календар для вибору правої дати */}
             <DatePicker
               selected={endDate}
               onChange={(date) => setEndDate(date)}
@@ -75,6 +88,7 @@ const Header = ({ title, onDateChange }) => {
               maxDate={new Date()} 
             />
             <button onClick={handleCancelButtonClick}>Скасувати</button>
+            {/* Підтвердження вибору дати з блокуванням кнопки при помилковому виборі */}
             <button
               onClick={handleConfirmButtonClick}
               disabled={endDate < startDate}
