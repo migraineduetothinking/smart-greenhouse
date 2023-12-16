002187fba5321becec91239213d7d2c8dc25ddee
Kyrylo Tereshchenko <terechenko.kirill@gmail.com>
2023-12-16 17:09:45 +0200

    Added a lines for min and max values in yaxis

--- src/functions/Chart.js
+++ src/functions/Chart.js
@@ -1,54 +1,114 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
+const Chart = ({ category, date }) => {
+  const minValue = Math.min(...category);
+  const maxValue = Math.max(...category);
 
-const Chart = ({ category, date }) =>  {
+  const roundedMinValue = minValue.toFixed(1);
+  const roundedMaxValue = maxValue.toFixed(1);
+
   const data = [
     {
       x: date,
       y: category,
       type: 'scatter',
       mode: 'lines+markers',
-      marker: { color: 'blue', size: 6 }, // Customize marker color and size
-      line: { 
-        color: 'darkblue', 
-        width: 1.5, 
-        shape: 'spline', // Use spline interpolation for a more curved line
-        
+      marker: { color: 'blue', size: 6 },
+      line: {
+        color: 'darkblue',
+        width: 1.5,
+        shape: 'spline',
       },
     },
   ];
 
-  
   const layout = {
-      title: ` `,
-      xaxis: {
+    title: ` `,
+    xaxis: {
       title: 'Дата',
       zeroline: false,
       tickangle: 35,
       tickformat: '%d/%m/%Y %H:%M:%S',
       tickmode: 'array',
-      range: [Math.min(...date), Math.max(...date)]
+      range: [Math.min(...date), Math.max(...date)],
+    },
+    yaxis: {
+      title: category,
+      zeroline: false,
+      rangemode: 'tozero',
+      fixedrange: true,
+      showline: false,
+    },
+    shapes: [
+      {
+        type: 'line',
+        xref: 'paper',
+        x0: 0,
+        x1: 1,
+        yref: 'y',
+        y0: minValue,
+        y1: minValue,
+        line: {
+          color: 'red',
+          width: 2,
+          dash: 'dash',
+        },
       },
-
-      yaxis: { title: category, zeroline: false, rangemode: 'tozero', fixedrange: true, },
-
-      width: 1240,
-      height: 650,
-      margin: { t: 50, b: 120 },
-      plot_bgcolor: '#f0f0f0',
-      paper_bgcolor: '#f0f0f0',
-      transition: {
+      {
+        type: 'line',
+        xref: 'paper',
+        x0: 0,
+        x1: 1,
+        yref: 'y',
+        y0: maxValue,
+        y1: maxValue,
+        line: {
+          color: 'red',
+          width: 2,
+          dash: 'dash',
+        },
+      },
+    ],
+    annotations: [
+      {
+        x: 0.5, // Center of the plot
+        y: minValue - 0.06 * (maxValue - minValue), // Adjust the multiplier as needed
+        xref: 'paper',
+        yref: 'y',
+        text: `Min: ${roundedMinValue}`,
+        showarrow: false,
+        font: {
+          color: 'red',
+        },
+      },
+      {
+        x: 0.5, // Center of the plot
+        y: maxValue + 0.06 * (maxValue - minValue), // Adjust the multiplier as needed
+        xref: 'paper',
+        yref: 'y',
+        text: `Max: ${roundedMaxValue}`,
+        showarrow: false,
+        font: {
+          color: 'red',
+        },
+      },
+    ],
+    width: 1240,
+    height: 650,
+    margin: { t: 50, b: 120 },
+    plot_bgcolor: '#f0f0f0',
+    paper_bgcolor: '#f0f0f0',
+    transition: {
       duration: 1000,
       easing: 'cubic-in-out',
-      },
-      
-   };
+    },
+  };
 
-   const chartConfig = {
-      displayModeBar: true,
-      responsive: true,
-    };
+  const chartConfig = {
+    displayModeBar: true,
+    responsive: true,
+  };
 
   return (
     <div className="sensor-category-container">
