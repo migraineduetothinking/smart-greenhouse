67eadffdb9d5ec1abce3d1b8f65b245d525c1684
Kyrylo Tereshchenko <terechenko.kirill@gmail.com>
2023-12-16 17:10:45 +0200

    Added a check whether starting date is smaller than ending date.

--- src/functions/DatePicker.js
+++ src/functions/DatePicker.js
@@ -1,5 +1,5 @@
 // DatePicker
-import React from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 
 const DatePicker = ({ dates, onSelectStartDate, onSelectEndDate }) => {
@@ -12,23 +12,44 @@ const DatePicker = ({ dates, onSelectStartDate, onSelectEndDate }) => {
     label: date,
   }));
 
+  const [startDateIndex, setStartDateIndex] = useState(null);
+  const [endDateIndex, setEndDateIndex] = useState(null);
+  const [dateError, setDateError] = useState(null);
+
   const handleStartDateChange = (selectedOption) => {
-    const startDate = uniqueDates[selectedOption.value];
-    onSelectStartDate(startDate);
+    const newStartDateIndex = selectedOption.value;
+    // Check if the start date is greater than the end date
+    if (endDateIndex !== null && newStartDateIndex > endDateIndex) {
+      // Display an error message
+      setDateError('Start date cannot be greater than end date');
+      return;
+    }
+    setStartDateIndex(newStartDateIndex);
+    setDateError(null);
+    onSelectStartDate(uniqueDates[newStartDateIndex]);
   };
 
   const handleEndDateChange = (selectedOption) => {
-    const endDate = uniqueDates[selectedOption.value];
-    onSelectEndDate(endDate);
+    const newEndDateIndex = selectedOption.value;
+    // Check if the end date is less than the start date
+    if (startDateIndex !== null && newEndDateIndex < startDateIndex) {
+      // Display an error message
+      setDateError('End date cannot be less than start date');
+      return;
+    }
+    setEndDateIndex(newEndDateIndex);
+    setDateError(null);
+    onSelectEndDate(uniqueDates[newEndDateIndex]);
   };
 
   return (
-   <div className="custom-date-picker">
+    <div className="custom-date-picker">
       <label>Дані від: </label>
       <Select options={options} onChange={handleStartDateChange} className="custom-select" />
       <label>До: </label>
       <Select options={options} onChange={handleEndDateChange} className="custom-select" />
-   </div>
+      {dateError && <p className="date-error">{dateError}</p>}
+    </div>
   );
 };
 
